6bbbe3ccf96cd575f8178f485a7d6be3c95bffa2
sebastian <js.millan10@uniandes.edu.co>
2018-09-20 00:31:57 -0500

    Esqueleto proyecto web arrglado

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Paths a los objetos Mongo
 var routes = require('./routes/index');
 
 var app = express();
