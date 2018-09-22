4327e65358e2d70a744638d7b5c8c9c1c866caf1
sebastian <js.millan10@uniandes.edu.co>
2018-09-21 20:22:15 -0500

    Todo el back, el csv y el creador de la tabla por json creado

--- frontend/src/components/Inicio.js
+++ frontend/src/components/Inicio.js
@@ -1,29 +1,88 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import vega from 'vega';
+import vegaLite from 'vega-lite';
+import csvParse from 'papaparse';
+import vegaEmbed from 'vega-embed';
 import registerServiceWorker from './../registerServiceWorker';
 
 export default class Inicio extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      texto: '',
+      embed: {},
+      spec: {},
+      archivo: null
+    };
+    this.handleUploadFile = this.handleUploadFile.bind(this);
     this.procesarArchivo = this.procesarArchivo.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.procesarTextoJson = this.procesarTextoJson.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
+    this.setState({embed: {'mode': 'vega-lite'}});
 
+    this.setState({
+      spec: {
+        '$schema': 'https://vega.github.io/schema/vega-lite/v2.json',
+        'description': 'A simple bar chart with embedded data.',
+        'data': {
+          'name': 'myData'
+        },
+        'mark': 'bar',
+        'encoding': {
+          'y': {'field': 'a', 'type': 'quantitative'},
+          'x': {'field': 'b', 'type': 'quantitative'}
+        }
+      }
+    });
+    // Arreglo de data prueba = [
+    //   {'a': 'A', 'b': 100}, {'a': 'B', 'b': 55}, {'a': 'C', 'b': 43},
+    //   {'a': 'D', 'b': 91}, {'a': 'E', 'b': 81}, {'a': 'F', 'b': 53},
+    //   {'a': 'G', 'b': 19}, {'a': 'H', 'b': 87}, {'a': 'I', 'b': 52}
+    // ];
   }
 
-  procesarArchivo(event)
-  {
-    var texto = event.target.value;
-    var obj = JSON.parse(texto)
-      .then((texto)=>{
-        console.log(texto);
-        // this.divTarget.value = JSON.stringify(obj,null, 2);
-      })
-      .catch((err)=>{
-        console.log(err);
-      });
+  handleUploadFile(e) {
+    var file = e.target.files[0];
+    console.log('archivo', file);
+    csvParse.parse(file, {
+      header: true,
+      complete: (results) => {
+        console.log('csv', results.data);
+        this.setState({archivo: results.data});
+        this.procesarArchivo();
+      }
+    });
+  }
+
+  procesarArchivo() {
+    try {
+      console.log('Json Archivo', this.state.archivo);
+      vegaEmbed(this.divTarget, this.state.spec, this.state.embed)
+        .then((res) => res.view.insert('myData', this.state.archivo).run());
+    }
+    catch (e) {
+      console.log('Problema archivo', e);
+    }
+  }
+
+  procesarTextoJson() {
+    try {
+      var json = JSON.parse(this.state.texto);
+      vegaEmbed(this.divTarget, this.state.spec, this.state.embed)
+        .then((res) => res.view.insert('myData', json).run());
+    }
+    catch (e) {
+      console.log('Problema escrito', e);
+    }
+  }
+
+  handleTextChange(event) {
+    this.setState({texto: event.target.value});
   }
 
   render() {
@@ -32,12 +91,12 @@ export default class Inicio extends Component {
         <div>
           Escriba el json que desea cargar
         </div>
-        <textarea cols='40' rows='20' onChange={this.procesarArchivo} ref={(div) => this.divTarget = div} >
-          Acá va el primer intento de json
-        </textarea>
-        <button onClick={this.procesarArchivo}>Cargar</button>
+        <textarea cols='40' rows='20' onChange={this.handleTextChange}/>
+        <br/>
+        <button onClick={this.procesarTextoJson}>Generar Tabla</button>
+        <span/><input type="file" onChange={this.handleUploadFile}/>
+        <div ref={(div) => this.divTarget = div}></div>
       </div>
-
     );
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,116 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://4c69d7f5-8cd5-4681-9602-c6c3090c946f:5230068c-68ba-4513-9766-7c357ea7dc7a@ds111113.mlab.com:11113/parcial1';
+const dbName = 'parcial1';
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('index', {title: 'Express'});
+});
+
+//Get graficas
+router.get('/graficas', (req, res, next) => {
+  MongoClient.connect(url, function (error, client) {
+    assert.equal(null, error);
+    console.log('Se conecto!');
+    const db = client.db(dbName);
+    let collection = db.collection('graficas');
+
+    collection.find().limit(20).toArray((err, result) => {
+      assert.equal(null, err);
+      console.log(result);
+      res.status(200).send(result);
+      client.close();
+    });
+  });
+});
+
+//Get de una sola grafica
+router.get('/graficas/:idGrafica', (req, res, next) => {
+  MongoClient.connect(url, function (error, client) {
+    assert.equal(null, error);
+    console.log('Se conecto');
+
+    const db = client.db(dbName);
+
+    let collection = db.collection('graficas');
+    let idGrafica = String(req.params.idGrafica);
+    collection.find({},
+      {
+        _id: new mongo.ObjectID(idGrafica)
+      }).toArray((err, result) => {
+      assert.equal(null, err);
+      console.log(result);
+      res.status(200).send(result);
+      client.close();
+    });
+  });
+
+});
+
+//Post graficas
+router.post('/graficas', (req, res, next) => {
+  MongoClient.connect(url, function (error, client) {
+    assert.equal(null, error);
+    console.log('Se conecto');
+
+    const db = client.db(dbName);
+    let collection = db.collection('graficas');
+
+    collection.insertOne({
+      autor: req.body.autor,
+      titulo: req.body.titulo,
+      grafica: req.body.grafica,
+      calificaciones: [],
+      timestamp: req.body.timestamp
+    },
+    (err, result) => {
+      assert.equal(null, err);
+      res.status(201).json({
+        message: 'Grafica guardada',
+      });
+
+      client.close();
+    });
+  });
+
+});
+
+//Add rating
+router.post('/graficas/:idGrafica', (req, res, next) => {
+  MongoClient.connect(url, function (error, client) {
+    assert.equal(null, error);
+    console.log('Se conecto');
+
+    const db = client.db(dbName);
+
+    let collection = db.collection('graficas');
+    let idGrafica = String(req.params.idGrafica);
+    collection.updateOne(
+      {
+        _id: new mongo.ObjectID(idGrafica)
+      },
+      {
+        $push: { calificaciones : {
+          puntaje: req.body.puntaje,
+          calificador: req.body.calificador
+        }
+        }
+      },
+      (err, result) => {
+        assert.equal(null, err);
+        res.status(201).json({
+          message: 'Se agrego la calificación',
+        });
+
+        client.close();
+      });
+  });
+
 });
 
 module.exports = router;
